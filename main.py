960c153b1f8b8f3635cec0ff529cb04560e8af88
mistahchris <mistahcummings@gmail.com>
2018-04-07 21:06:14 -0500

    clean up cra base project for blank slate

--- main.py
+++
@@ -1,4 +0,0 @@
-from api import server
-
-if __name__ == '__main__':
-    server.app.run(port=8888)
--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="">
+        <header className="">
+          <h1 className="">hello</h1>
         </header>
       </div>
     );
